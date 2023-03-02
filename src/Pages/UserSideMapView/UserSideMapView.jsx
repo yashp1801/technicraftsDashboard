414ff76c9c3bddf5fa72c86a293219fad4e2a21e
Yash Patil <yash.p1801@gmail.com>
2023-03-02 19:16:44 +0530

    commit

--- src/Pages/UserSideMapView/UserSideMapView.jsx
+++ src/Pages/UserSideMapView/UserSideMapView.jsx
@@ -13,10 +13,9 @@ import Loader from "../../scenes/global/Loader";
 const center = { lat: 20.5937, lng: 78.9629 };
 
 const data = [
-  
   {
     id: 1,
-    lat: 19.0330,
+    lat: 19.033,
     lng: 73.0297,
   },
 ];
@@ -61,6 +60,7 @@ const MapView = () => {
         zoom={6}
         mapContainerStyle={{ width: "100%", height: "100%" }}
       >
+        <div>hey this is map</div>
         {data.map((item) => {
           return (
             <MarkerF
